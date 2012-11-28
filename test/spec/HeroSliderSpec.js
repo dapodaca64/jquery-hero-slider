87fc1fe826a6184c9ce6560551fa53c6fc1aad9e
Daniel Apodaca <daniel.apodaca@designkitchen.com>
2012-11-28 17:23:57 -0600

    Add some Specs for the HeroSlider

---
+++ test/spec/HeroSliderSpec.js
@@ -0,0 +1,51 @@
+describe("Hero", function(){
+  var hero;
+
+  beforeEach(function(){
+    hero = new HeroSlider();
+  });
+
+  describe("when in summary state", function(){
+
+    it("should allow navigation to the next story", function(){
+    });
+
+    it("should allow navigation to the previous story", function(){
+    });
+
+    it("should update story index indicator to the current story", function(){
+    });
+
+    it("should allow navigation to the detail state of a current story", function(){
+    });
+
+    it("should autorotate on landing", function(){
+    });
+
+    it("should allow for animations to be stopped", function(){
+    });
+
+    it("should run only the current animation", function(){
+    });
+
+    it("should stop all animation when starting a new animation", function(){
+    });
+
+
+  });
+
+  describe("when in detail state", function(){
+
+  });
+
+  describe("when animations are stopped", function(){
+
+    it("should indicate that animations are stopped", function(){
+    });
+
+    it("should should be possible resume", function(){
+    });
+
+  });
+
+});
