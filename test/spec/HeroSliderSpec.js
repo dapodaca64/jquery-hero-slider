6a9315a6ea406aff2db0f443ccbcca278dc186ed
Daniel Apodaca <daniel.apodaca@gmail.com>
2012-11-29 23:56:22 -0600

    Updated HeroSlider specs

--- test/spec/HeroSliderSpec.js
+++ test/spec/HeroSliderSpec.js
@@ -1,50 +1,278 @@
-describe("Hero", function(){
-  var hero;
+describe("Hero Slider", function(){
+  var hero, idOffset = 0;
 
   beforeEach(function(){
-    hero = new HeroSlider();
+
+    var el = document.createElement("div");
+    // Ensure unique DOM ids
+    var elId = "hero_slider_"+idOffset;
+    idOffset += 1;
+
+    el.setAttribute("id", elId);
+    el.setAttribute("class", "hero-slider");
+    document.body.appendChild(el);
+
+    $el = $("#"+elId);
+    $el.css({
+      "width": "100px",
+      "height": "100px",
+      "position": "absolute",
+      "top": "200px",
+      "left": "800px",
+      "background-color": "green",
+      "display": "none",
+      "font-family": "Arial",
+      "font-size": "12px",
+      "line-height": "14px",
+      "color": "#ffffff",
+      "padding": "8px"
+    });
+
+    hero = new HeroSlider({
+      el: $el
+    });
+
+    hero.init();
+
+    hero.newAnimatedElement({
+      el: hero.el,
+      animateDuration: 3000+topOffset*10,
+      animateCallback: function() {
+        //removeAnimatedObject();
+      }
+    });
+
+
   });
 
-  describe("when in summary state", function(){
+  afterEach(function(){
+    //this removes the DOM object before animations are complete. So
+    //we manage this in the animation callback below
+    //$("#hero_slider").remove();
+  });
 
-    it("should allow navigation to the next story", function(){
+  var topOffset = 0;
+  var incrementTopPosition = function($el) {
+    topOffset += 120;
+    $el.css({ "top": topOffset });
+    return $el;
+  }
+  var markDebugEl = function($el, message) {
+    $el.html(message);
+    incrementTopPosition($el);
+    $el.show()
+  };
+  var removeAnimatedObject = function(){
+    $("#hero_slider").remove();
+  };
+
+  describe("when running with default configuration", function(){
+
+    it("should allow switching between layout modes 'default' and 'detail'", function(){
+      //default is "default"
+
+      //switch to "detail"
+      hero.layoutModeToggle();
+      var a = hero.layoutMode;
+      expect(a).toBe("detail");
+
+      //switch back to "default"
+      hero.layoutModeToggle();
+      var b = hero.layoutMode;
+      expect(b).toBe("default");
     });
-
-    it("should allow navigation to the previous story", function(){
-    });
-
-    it("should update story index indicator to the current story", function(){
-    });
-
-    it("should allow navigation to the detail state of a current story", function(){
-    });
-
-    it("should autorotate on landing", function(){
-    });
-
-    it("should allow for animations to be stopped", function(){
-    });
-
-    it("should run only the current animation", function(){
-    });
-
-    it("should stop all animation when starting a new animation", function(){
-    });
-
 
   });
 
-  describe("when in detail state", function(){
+  describe("when configured to autorotate", function(){
+
+    it("should be animating on landing", function(){
+      console.log(arguments);
+      //animation test will show the related DOM element created in the setup beforeEach
+      markDebugEl($el, "animate if autorotate");
+
+      //set configuration
+      hero.autoRotate = true;
+
+      //initiate the landing state
+      hero.run();
+
+      var a = hero.isPlaying();
+      expect(a).toBe(true);
+    });
+
+  });
+
+  describe("when configured not to autorotate", function(){
+
+    it("should be static (not animating) upon landing", function(){
+      //animation test will show the related DOM element created in the setup beforeEach
+      markDebugEl($el, "no animate if not autorotate");
+
+      //set configuration
+      hero.autoRotate = false;
+
+      //initiate the landing state
+      hero.run();
+
+      var a = hero.isPlaying();
+      expect(a).toBe(false);
+    });
+
+  });
+
+  describe("when navigating", function(){
+
+    it("should always be able to go to a next story", function(){
+      //3 stories,
+      hero.storyCount = 3;
+
+      //starting at story 1 (zero-indexed):
+      hero.storyIndex = 0;
+
+      //A: go to story 2
+      hero.nextStory();
+      var a = hero.storyIndex+1;
+      expect(a).toBe(2);
+
+      //B: go to story 3
+      hero.nextStory();
+      var b = hero.storyIndex+1;
+      expect(b).toBe(3);
+
+      //C: wrap around to story 1
+      hero.nextStory();
+      var c = hero.storyIndex+1;
+      expect(c).toBe(1);
+    });
+
+    it("should always be able to go to a previous story", function(){
+      //3 stories,
+      hero.storyCount = 3;
+
+      //starting at story 1 (zero-indexed):
+      hero.storyIndex = 0;
+
+      //A. wrap around to story 3
+      hero.previousStory()
+      var a = hero.storyIndex+1;
+      expect(a).toBe(hero.storyCount);
+
+      //B. go to story 2
+      hero.previousStory()
+      var b = hero.storyIndex+1;
+      expect(b).toBe(2);
+
+      //C. go to story 1
+      hero.previousStory()
+      var c = hero.storyIndex+1;
+      expect(c).toBe(1);
+    });
 
   });
 
   describe("when animations are stopped", function(){
 
     it("should indicate that animations are stopped", function(){
+      //animation test will show the related DOM element created in the setup beforeEach
+      markDebugEl($el, "should indicate that animations are stopped");
+      incrementTopPosition($el);
+      $el.show()
+
+      hero.stopAnimation();
+      var a = hero.isPlaying();
+      expect(a).toBe(false);
     });
 
-    it("should should be possible resume", function(){
+    it("should allow for animations to be started", function(){
+      //animation test will show the related DOM element created in the setup beforeEach
+      markDebugEl($el, "should allow for animations to be started");
+
+      hero.startAnimation();
+      var a = hero.isPlaying();
+      expect(a).toBe(true);
     });
+
+    it("should be possible restart animations", function(){
+      //animation test will show the related DOM element created in the setup beforeEach
+      markDebugEl($el, "should be possible to restart animations");
+
+      hero.stopAnimation();
+      hero.startAnimation();
+      var a = hero.isPlaying();
+      expect(a).toBe(true);
+    });
+
+  });
+
+  describe("when animations are running", function(){
+
+    it("should indicate that animations are running", function(){
+      //animation test will show the related DOM element created in the setup beforeEach
+      markDebugEl($el, "should indicate that animations are running");
+
+      incrementTopPosition($el);
+      $el.show()
+
+      hero.startAnimation();
+      var a = hero.isPlaying();
+      expect(a).toBe(true);
+    });
+
+    it("should allow for animations to be stopped", function(){
+      //animation test will show the related DOM element created in the setup beforeEach
+      markDebugEl($el, "should allow for animations to be stopped");
+
+      incrementTopPosition($el);
+      $el.show()
+
+      hero.startAnimation();
+      hero.stopAnimation();
+      var a = hero.isPlaying();
+      expect(a).toBe(false);
+    });
+
+    it("should be possible restart animations", function(){
+      //animation test will show the related DOM element created in the setup beforeEach
+      markDebugEl($el, "should be possible to restart animations");
+
+      hero.startAnimation();
+      hero.stopAnimation();
+      hero.startAnimation();
+      var a = hero.isPlaying();
+      expect(a).toBe(true);
+    });
+
+  });
+
+  describe("when stacking animations", function(){
+
+    it("should run only the current animation", function(){
+      // Alternately:
+      // "should dequeue all animation when starting a new animation"
+
+      //animation test will show the related DOM element created in the setup beforeEach
+      markDebugEl($el, "should run only the current animation");
+
+      // Start asynchronous animations one after another
+      hero.startAnimation();
+      hero.startAnimation();
+
+      // The second animation call would normally stack
+      // animations into the queue but it should
+      // dequeue animations with every start.
+      // let's get the collection of animation queues and
+      // check that each queue has one animation in it
+      var anims = hero.getAnimationQueues();
+      for (var i=0, j=anims.length; i<j; i++) {
+        var a = anims[i].length;
+        expect(a).toBe(1);
+      }
+    });
+
+  });
+
+  xdescribe("when in detail layout mode", function(){
 
   });
 
